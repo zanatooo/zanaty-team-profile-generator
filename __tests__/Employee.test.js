02fae8d80a84175ba4d794e251e8ce3bfe749e01
zanatooo <m8elzanaty@gmail.com>
2022-05-21 19:59:57 -0700

    correcting the tests files

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,27 +1,27 @@
 const Employee = require ("../Lib/Employee")
 
 test("Test Employee Name", function(){
-    const newHire = new Employee("Erik",222,"email@test.com")
-    expect(newHire.name).toBe("Erik")
+    const newHire = new Employee("Mo",1111,"email@test.com")
+    expect(newHire.name).toBe("Mo")
 })
 test("Test Employee Id", function(){
-    const newHire = new Employee("Erik",222,"email@test.com")
-    expect(newHire.id).toBe(222)
+    const newHire = new Employee("Mo",1111,"email@test.com")
+    expect(newHire.id).toBe(1111)
 })
 test("Test Employee Email", function(){
-    const newHire = new Employee("Erik",222,"email@test.com")
+    const newHire = new Employee("Mo",1111,"email@test.com")
     expect(newHire.email).toBe("email@test.com")
 })
 
 test("Test Employee getName", function(){
-    const newHire = new Employee("Erik",222,"email@test.com")
-    expect(newHire.getName()).toBe("Erik")
+    const newHire = new Employee("Mo",1111,"email@test.com")
+    expect(newHire.getName()).toBe("Mo")
 })
 test("Test Employee getId", function(){
-    const newHire = new Employee("Erik",222,"email@test.com")
-    expect(newHire.getId()).toBe(222)
+    const newHire = new Employee("Mo",1111,"email@test.com")
+    expect(newHire.getId()).toBe(1111)
 })
 test("Test Employee getEmail", function(){
-    const newHire = new Employee("Erik",222,"email@test.com")
+    const newHire = new Employee("Mo",1111,"email@test.com")
     expect(newHire.getEmail()).toBe("email@test.com")
 })
